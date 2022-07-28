73d308dcaffb6211f48edc1ee35969a26c09c863
frederikprijck <frederik.prijck@auth0.com>
2022-07-28 15:08:07 +0200

    Fix linter

--- sample/lib/hero.dart
+++ sample/lib/hero.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'constants.dart';
 
 final Shader linearGradient = const LinearGradient(colors: <Color>[
