66a92947bd5c9d57aaf3a5e006e6a2399fb89f5d
frederikprijck <frederik.prijck@auth0.com>
2022-07-28 15:06:09 +0200

    fix linter

--- sample/lib/hero.dart
+++ sample/lib/hero.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'constants.dart';
+
+final Shader linearGradient = const LinearGradient(colors: <Color>[
+  Color.fromRGBO(255, 79, 64, 100),
+  Color.fromRGBO(255, 68, 221, 100)
+], begin: Alignment.topLeft, end: Alignment.bottomRight)
+    .createShader(const Rect.fromLTWH(0.0, 0.0, 500.0, 70.0));
 
 class HeroWidget extends StatelessWidget {
   const HeroWidget({Key? key}) : super(key: key);
@@ -17,9 +24,9 @@ class HeroWidget extends StatelessWidget {
               child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text('Flutter'),
-                    const Text('Sample App'),
+                  children: const [
+                    Text('Flutter'),
+                    Text('Sample App'),
                   ])))
     ]);
   }
