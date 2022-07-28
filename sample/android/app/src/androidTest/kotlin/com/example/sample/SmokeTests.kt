e06d75d6cf587b47d0738805e1fe3498dc5cf1c6
frederikprijck <frederik.prijck@auth0.com>
2022-07-28 13:51:06 +0200

    update screenshots

--- sample/android/app/src/androidTest/kotlin/com/example/sample/SmokeTests.kt
+++ sample/android/app/src/androidTest/kotlin/com/example/sample/SmokeTests.kt
@@ -50,7 +50,8 @@ class SmokeTest {
             addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK)
         }
         context.startActivity(intent)
-        externalsDir = Environment.getExternalStoragePublicDirectory(DIRECTORY_PICTURES) ?: File("/sdcard/Pictures");
+        externalsDir = context.getExternalFilesDir(DIRECTORY_PICTURES);
+        //externalsDir = Environment.getExternalStoragePublicDirectory(DIRECTORY_PICTURES) ?: File("/sdcard/Pictures");
 
         // Wait for the app to appear
         device.wait(Until.hasObject(By.pkg(PACKAGE_NAME).depth(0)), TIMEOUT)
